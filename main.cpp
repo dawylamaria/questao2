bf06e38b338f3a119aca45573fee49ce2b693783
Dawyla Maria <dawylamaria@gmail.com>
2023-08-15 12:47:41 -0300

    questao 2

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* ques2*/
+using namespace std;
+int main(int argc, char** argv) {
+	int num1;
+	cout <<"Digite um numero:" << endl;
+	
+	cin >> num1;
+	
+	cout << "Numero digitado:" <<num1 << endl;
+	
+	system("pause");
+	return 0;
+}
